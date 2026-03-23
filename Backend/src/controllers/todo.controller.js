ee832347a81ad1d8b6f0c053e70adcb3ab720671
Vishvajeet Sharma <vsharma6705@gmail.com>
2026-03-24 01:09:28 +0530

    compelete get tasks api

--- Backend/src/controllers/todo.controller.js
+++ Backend/src/controllers/todo.controller.js
@@ -1,5 +1,5 @@
 import { sendResponse } from "../utils/response.js";
-import {TodoModel} from "../models/todo.model.js";
+import { TodoModel } from "../models/todo.model.js";
 
 export const createTodo = async (req, res) => {
   try {
@@ -11,13 +11,27 @@ export const createTodo = async (req, res) => {
       description,
       dueDate,
       userId
-    };    
-    
+    };
+
     const savedTodo = await TodoModel.create(newTodo);
-    if(savedTodo){
+    if (savedTodo) {
       return sendResponse(res, true, 201, "Todo created successfully", savedTodo, false);
     }
   } catch (error) {
-    return sendResponse(res, false, 500, error || "Internal Server Error", [], true);
+    return sendResponse(res, false, 500, error.message || "Internal Server Error", [], true);
+  }
+}
+
+export const getTodos = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+
+    const todos = await TodoModel.find({ userId }).sort({ createdAt: -1 });
+    if (!todos || todos.length === 0) {
+      return sendResponse(res, true, 200, "No todos found", [], false);
+    }
+    return sendResponse(res, true, 200, "Todos fetched successfully", todos, false);
+  } catch (error) {
+    return sendResponse(res, false, 500, error.message || "Internal Server Error", [], true);
   }
 }
--- Backend/src/routes/todo.router.js
+++ Backend/src/routes/todo.router.js
@@ -1,11 +1,12 @@
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import validateTodo from "../middlewares/validateTodo.js";
-import { createTodo } from "../controllers/todo.controller.js";
+import { createTodo, getTodos } from "../controllers/todo.controller.js";
 
 
 const router = express.Router();
 
 router.post(`/create-todo`, authMiddleware, validateTodo, createTodo);
+router.get(`/get-todos`, authMiddleware, getTodos);
 
 export default router; 
